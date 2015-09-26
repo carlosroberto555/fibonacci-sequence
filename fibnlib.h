4a2d586745cc4a9c3586ab1ba445e873d215a62c
Carlos Roberto <carlos-roberto_2013@outlook.com>
2015-09-26 01:13:43 -0300

    Update fibnlib.h

--- fibnlib.h
+++ fibnlib.h
@@ -1,21 +1,3 @@
-/*
-	Função que verifica se um valor pertence à sequência de Fibonacci.
-	Sequencia de Fibonacci f(N) = { 1,1,2,3,5,8,13,21,34,55,89,144,233,377,...)
-	É formada pela soma de um número com seu anterior que pertence à sequencia,
-	Começando do 1, ou seja, {1+1=2, 2+1=3, 3+2=5, ...}
-	
-	Exemplo de utilização:
-		
-		if(fibn(variavel))
-		{
-			printf("Pertence!");
-		}
-		else
-		{
-			printf("Não pertence!");
-		}
-*/
-
 bool fibn(int n)
 {
 	int seqFi[3] = {1,1,0}; // Inicialização do vetor
