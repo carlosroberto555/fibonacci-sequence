1c71676d4e8e74f45e52976c8bd40f0ab4d0ad85
Carlos Roberto <carlosrpj555@gmail.com>
2020-11-18 01:38:08 -0300

    Update fibnlib.h
    refactoring

--- fibnlib.h
+++ fibnlib.h
@@ -1,22 +1,23 @@
-bool fibn(int n)
+bool fibn(int num)
 {
-	int seqFi[3] = {1,1,0}; // Inicialização do vetor
-	bool pertc = false;
+    int seqFi[3] = { 1, 1, 0 }; // Inicialização do vetor
 
-	while(seqFi[2] <= n)
-	{
-		// Aqui ocorre a soma com o anterior
-		seqFi[2] = seqFi[0]+seqFi[1];
+    while(seqFi[2] <= num)
+    {
+        // Aqui ocorre a soma com o anterior
+        seqFi[2] = seqFi[0] + seqFi[1];
 
-		// Se o número informado for igual ao corrente, pertc recebe true
-		if (n == seqFi[1])
-			pertc = true;
-		
-		// Aqui ocorre a troca dos valores para testar o próximo
-		seqFi[0] = seqFi[1];
-		seqFi[1] = seqFi[2];
-	}
-	
-	// Retorna true se pertencer, ou false se não pertencer à sequência
-	return pertc;
+        // Se o número informado for igual ao corrente, retorna true
+        if (num == seqFi[1]) {
+            // (PERTENCE)
+            return true;
+        }
+
+        // Aqui ocorre a troca dos valores para testar o próximo
+        seqFi[0] = seqFi[1];
+        seqFi[1] = seqFi[2];
+    }
+
+    // Se chegar aqui é porque não pertence
+    return false;
 }
